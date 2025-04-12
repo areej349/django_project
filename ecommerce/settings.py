b250916818258637af897627da01132739fc3663
areej349 <158787695+areej349@users.noreply.github.com>
2025-04-13 01:02:28 +0300

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME' : 'ecommerce' , 
-        'USER' : 'root',
-        'PASSWORD': '123456',
+        'NAME' : '' , 
+        'USER' : '',
+        'PASSWORD': '',
         'HOST' : 'localhost',
-        'PORT' : '3306' ,
+        'PORT' : '' ,
     }
 }
 
